98139d37d437398e42cf6c84d0beea214cbb1c3f
gonzaloiniesta <Benicassim1999>
2020-10-26 21:26:51 +0100

    entregable 1 empezado

---
+++ Entregables/Entregable1/entregable1.py
@@ -0,0 +1,17 @@
+from typing import *
+
+Vertex = Tuple[int,int]
+Edge = Tuple[Vertex,Vertex]
+
+def read_file(f):
+    linea = f.readline()
+    linea = linea.split(" ")
+    return linea
+
+def create_labyring():
+    return
+
+
+if __name__ == '__main__':
+    rows, cols, forbidden_edges = 0
+
