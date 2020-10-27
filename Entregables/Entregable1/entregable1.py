10f7155bb7ac6b34a74a86a49f60908c13725e02
gonzaloiniesta <Benicassim1999>
2020-10-27 12:18:26 +0100

    leer fichero

--- Entregables/Entregable1/entregable1.py
+++ Entregables/Entregable1/entregable1.py
@@ -4,14 +4,31 @@ Vertex = Tuple[int,int]
 Edge = Tuple[Vertex,Vertex]
 
 def read_file(f):
-    linea = f.readline()
-    linea = linea.split(" ")
-    return linea
+    tuplas_prohibidas = set()
+    rows = int(f.readline().split(" ")[0])
+    f.seek(0)
+    cols = int(f.readline().split(" ")[1])
+    n_f_edges = int(f.readline().split(" ")[0])
+    i = 0
+    while i < n_f_edges:
+        linea = f.readline().rstrip('\n').split(" ")
+        tupla = ((int(linea[0]), int(linea[1])), (int((linea[2])), int(linea[3])))
+        tuplas_prohibidas.add(tupla)
+        i += 1
+    return rows,cols,tuplas_prohibidas
+
 
 def create_labyring():
     return
 
 
 if __name__ == '__main__':
-    rows, cols, forbidden_edges = 0
+    file = open("lab_2_4_ok.ins", "r")
+    info = read_file(file)
+    rows = info[0]
+    cols = info[1]
+    tuplas_prohibidas = info[2]
+
+
+    print(rows,cols,tuplas_prohibidas)
 
