6d1eb47f5205918df6cb2cfd012b96ca2d1f05f9
gonzaloiniesta <Benicassim1999>
2020-11-03 15:39:59 +0100

    Forma de leer datos

--- Entregables/Entregable1/entregable1.py
+++ Entregables/Entregable1/entregable1.py
@@ -247,10 +247,10 @@ if __name__ == '__main__':
         if (sys.argv[1] == "-g"):
 
             # Obligatorio: Crea un LabyrinthViewer pasándole el grafo del laberinto
-            lv = LabyrinthViewer(graph, canvas_width=800, canvas_height=600, margin=10)
+            #lv = LabyrinthViewer(graph, canvas_width=800, canvas_height=600, margin=10)
 
             # Obligatorio: Muestra el laberinto
-            lv.run()
+            #lv.run()
 
             print("Laberinto pintado con exito")
 
