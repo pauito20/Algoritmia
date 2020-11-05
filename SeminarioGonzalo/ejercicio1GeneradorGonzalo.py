054fd977417c5b4f7bf8ada3a61f39eb9aa175bd
gonzaloiniesta <Benicassim1999>
2020-11-05 19:20:06 +0100

    seminarios

---
+++ SeminarioGonzalo/ejercicio1GeneradorGonzalo.py
@@ -0,0 +1,74 @@
+
+import math
+
+#Ejercicio1
+
+def sec_cuadrado(l):
+    for i in l:
+        yield i*i
+
+
+#Ejercicio2
+
+def first ( n : int, it ):
+    j = 0
+    for i in it:
+        if j < n:
+            yield i
+            j +=1
+
+#Ejercicio3
+
+def filter(cond, it):
+    for i in it:
+        if cond(i):
+            yield i
+
+#Ejercicio4
+
+def take_while(cond, it):
+    for i in it:
+        if not cond(i):
+           yield i
+
+#Ejercicio5
+
+def squares():
+    i = 0
+    while True:
+        if math.sqrt(i):
+            yield i * i
+        i += 1
+
+
+
+if __name__ == '__main__':
+
+
+    print("Ejercio 1: ")
+    for sq in sec_cuadrado([1, 2, 10, 4, 5]):
+        print(sq)
+
+    print("\n Ejercicio 2: ")
+    for i in first(20, range(50,200)):
+        print(i)
+    print("------------")
+    for i in first(100, [2,4,5,7,2]):
+        print(i)
+
+    print("\n Ejercicio 3: ")
+    for i in filter(lambda n: n < 100, range(50,200)):
+        print(i)
+    print("------------")
+    for i in filter(lambda n : n%2 == 0, [2,4,5,7,2]):
+        print(i)
+
+
+    for i in take_while(lambda n: n < 100, range(50, 200)):
+        print(i)
+    print("------------")
+    for i in take_while(lambda n: n % 2 == 0, [2, 4, 5, 7, 2]):
+        print(i)
+    print("\n Ejercicio 4: ")
+    for i in squares():
+        print(i)
--- SeminarioPau/ejercicio1GeneradorPau.py
+++ SeminarioPau/ejercicio1GeneradorPau.py
@@ -1,11 +1,70 @@
+import math
 
 def sec_cuadrado(l):
     for i in l:
-        yield i*i
+        yield i * i
+
+
+def first(n, it):
+    max = 0
+    for i in it:
+        if max <= n:
+            yield i
+            max += 1
+
+
+def filter(cond, it):
+    for i in it:
+        if cond(i):
+            yield i
+
+
+def take_while(cond, it):
+    for i in it:
+        if not cond(i):
+            yield i
+
+
+def squares():
+    p = 1
+    while True:
+        if math.sqrt(p):
+            yield p * p
+        p += 1
 
 
 if __name__ == '__main__':
 
-    lista = [1,2,3,4,5]
+    lista = [1, 2, 3, 4, 5]
+    print("Probamos sec_cuadrado")
     for sq in sec_cuadrado(lista):
         print(sq)
+
+    print("Probamos first con range")
+    for elem in first(20, range(50, 200)):
+        print(elem)
+
+    print("Probamos first con lista")
+    for elem in first(100, [2, 4, 5, 7, 2]):
+        print(elem)
+
+    print("Probamos filter con range")
+    for fil in filter(lambda n: n < 100, range(50, 200)):
+        print(fil)
+
+    print("Probamos filter con lista")
+    for fil in filter(lambda n: n % 2 == 0, [2, 4, 5, 7, 2]):
+        print(fil)
+
+    print("Probamos take_while con range")
+    for fil in take_while(lambda n: n < 100, range(50, 200)):
+        print(fil)
+
+    print("Probamos take_while con lista")
+    for fil in take_while(lambda n: n % 2 == 0, [2, 4, 5, 7, 2]):
+        print(fil)
+
+    print("Probamos squares")
+    for per_sq in squares():
+        print(per_sq)
+
