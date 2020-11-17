81f2aca2764a07308d079a7899b38ea0d96eb5cf
gonzaloiniesta <Benicassim1999>
2020-11-17 17:57:15 +0100

    Practica 3 acabada

--- Practicas/Problemas/Practica3Gonzalo/knapsackGonzalo.py
+++ Practicas/Problemas/Practica3Gonzalo/knapsackGonzalo.py
@@ -4,25 +4,34 @@ from random import random, seed
 
 def knapsack_solve(weights, values, capacity):
     class KnapsackPS(PartialSolutionWithOptimization):
-        def __init__(self):         # IMPLEMENTAR: Añade los parámetros que tú consideres
-            pass
+        def __init__(self, pendiente: int, usados : Tuple[int, ...], valor : int): # IMPLEMENTAR: Añade los parámetros que tú consideres
+            self.pendiente = pendiente    #lo que falta para completar la machila
+            self.usados = usados    #decisiones tomadas
+            self.n = len(usados)    #longitud de las decisiones tomadas
+            self.valor = valor      #valor de la mochila
 
         def is_solution(self) -> bool:      # IMPLEMENTAR
-            pass
+            return self.n == len(values)
 
         def get_solution(self) -> Solution: # IMPLEMENTAR
-            pass
+            return self.usados
 
         def successors(self) -> Iterable["KnapsackPS"]:# IMPLEMENTAR
-            pass
+            if self.n < len(weights):
+                yield KnapsackPS(self.pendiente, self.usados + (0,), self.valor)
+                if weights[self.n] <= self.pendiente:
+                    newPendiente = self.pendiente - weights[self.n]
+                    newValor =  self.valor + values[self.n]
+                    yield KnapsackPS(newPendiente, self.usados + (1,), newValor)
 
         def state(self) -> State:           # IMPLEMENTAR
-            pass
+            return (self.pendiente, self.n)
 
-        def f(self) -> Union[int, float]:   # IMPLEMENTAR
-            pass
+        def f(self) -> Union[int, float]:   #Es la funcion que queremos optimizar
+            return -self.valor
+            #return -sum(self.usados[i] * values[i] for i in range(len(self.usados)))     #obtenemos elm valor de la mochila ( el - para maximizar )
 
-    initialPS = KnapsackPS()                # IMPLEMENTAR: Añade los parámetros que tú consideres
+    initialPS = KnapsackPS(capacity, (), 0)                # IMPLEMENTAR: Añade los parámetros que tú consideres
     return BacktrackingOptSolver.solve(initialPS)
 
 def create_knapsack_problem(num_objects: int) -> Tuple[Tuple[int,...], Tuple[int,...], int]:
@@ -38,5 +47,8 @@ if __name__ == "__main__":
     W, V, C = [1, 4, 2, 3], [2, 3, 4, 2], 7     # SOLUCIÓN: Weight=7,    Value=9
     # W, V, C = create_knapsack_problem(30)     # SOLUCIÓN: Weight=6313, Value=11824
     for sol in knapsack_solve(W, V, C):
+        w = sum(W[i] * m for i, m in enumerate(sol))
+        v = sum(V[i] * m for i, m in enumerate(sol))
+        print(f"Weight = {w} Values ={v}")
         print (sol)
     print("\n<TERMINADO>")
