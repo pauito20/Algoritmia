dd3057c90f816634a38a34dd09d57140930f77d8
pauito20 <al386012@uji.es>
2020-12-09 22:17:02 +0100

    recursividad falta

--- Entregables/Entregable4/entregable4.py
+++ Entregables/Entregable4/entregable4.py
@@ -3,12 +3,18 @@ from typing import List, Optional
 
 
 def funambulista(edificios: List[int]) -> Optional[List[int]]:
-    def funambilistaRecursive():
-        pass
+    def funambilistaRecursive(edificios: List[int],i_ed_peq: int, i_ed_gran: int,i_ed_valle: int  ) -> Optional[List[int]]:
+        #Caso base
+        if len(edificios) <= 1:
+            return []
+        #Recursividad
+        elif
+            pass
 
 
 
-
+    #Llamada de la función recursiva inicial
+    return funambilistaRecursive(edificios, -1, -1, -1 )
 
 
 if __name__ == '__main__':
@@ -20,8 +26,18 @@ if __name__ == '__main__':
     for i in range(1, len(lineas_fich)):
         alturas.append(int(lineas_fich[i]))
 
-
+    print("\n----- Parámetros de entrada -----")
     print("Nº de edificios: ", numEdificios)
     print("Alturas de los edificios: ", alturas)
+
+    res = funambulista(alturas)
+    print("\n----- RESULTADO -----")
+
+    if len(res) == 0:
+        print("NO HAY SOLUCIÓN")
+    else:
+        print("El resultado es: ", res)
+
+
 
 
