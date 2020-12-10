4e022f48bc82bd4157bf2d0e8ffb14bc97cbfe13
gonzaloiniesta <Benicassim1999>
2020-12-10 13:17:41 +0100

    lectura

--- Entregables/Entregable4/entregable4.py
+++ Entregables/Entregable4/entregable4.py
@@ -1,23 +1,43 @@
+import os
 import sys
 from typing import List, Optional
 
 
 def funambulista(edificios: List[int]) -> Optional[List[int]]:
-    def funambilistaRecursive(edificios: List[int],i_ed_peq: int, i_ed_gran: int,i_ed_valle: int  ) -> Optional[List[int]]:
+    def funambilistaRecursive(i_ed_peq: int, i_ed_gran: int,i_ed_valle: int  ) -> Optional[List[int]]:
         #Caso base
         if len(edificios) <= 1:
             return []
         #Recursividad
-        elif
-            pass
+
 
 
 
     #Llamada de la función recursiva inicial
-    return funambilistaRecursive(edificios, -1, -1, -1 )
-
+    return funambilistaRecursive(-1,-1,-1)
 
 if __name__ == '__main__':
+
+
+
+    name_fich = input("Introduce el nombre(ruta) del fichero: ")
+
+    if not os.path.isfile(name_fich):
+        print("El parametro introducido(", name_fich, ") no es un fichero.")
+        exit(0)
+
+
+    file = open(name_fich, "r")
+    linea = str(file.readline())
+    numEdificios = int(linea)
+
+    alturas = []
+    for i in range(numEdificios):
+        linea = (str(file.readline().rstrip('\n')))
+        alturas.append(linea)
+
+    '''
+    
     # Convertimos el fichero en una lista de líneas
     lineas_fich = sys.stdin.readlines()
     numEdificios = int(lineas_fich[0])
@@ -25,19 +45,20 @@ if __name__ == '__main__':
     alturas = []
     for i in range(1, len(lineas_fich)):
         alturas.append(int(lineas_fich[i]))
+    '''
 
     print("\n----- Parámetros de entrada -----")
     print("Nº de edificios: ", numEdificios)
     print("Alturas de los edificios: ", alturas)
-
+    '''
     res = funambulista(alturas)
     print("\n----- RESULTADO -----")
-
+    
     if len(res) == 0:
         print("NO HAY SOLUCIÓN")
     else:
         print("El resultado es: ", res)
 
-
+    '''
 
 
